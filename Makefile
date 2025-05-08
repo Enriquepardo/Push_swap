3ba95b5c08e16b1d0210a40dbf15124bd70e94c7
Enriquepardo <quiquepardos@gmail.com>
2025-05-08 21:40:32 +0200

    WIP

--- Makefile
+++ Makefile
@@ -1,24 +1,33 @@
+# Makefile - raíz del proyecto push_swap
+
 NAME = push_swap
 
 CC = cc
-CFLAGS = -Wall -Wextra -Werror -I./inc
+CFLAGS = -Wall -Wextra -Werror -I./inc -Isrc/libft/inc
+
 SRC_DIR = src
 INC_DIR = inc
+LIBFT_DIR = src/libft/src
+LIBFT = $(LIBFT_DIR)/libft.a
 
 SRC = $(SRC_DIR)/push_swap.c \
       $(SRC_DIR)/stack_operations.c \
       $(SRC_DIR)/stack_utils.c \
       $(SRC_DIR)/sorting.c \
       $(SRC_DIR)/input_validation.c \
+      $(SRC_DIR)/quick_sort.c \
       $(SRC_DIR)/utils.c \
-	  $(SRC_DIR)/radix_utils.c
+      $(SRC_DIR)/radix_utils.c
 
 OBJ = $(SRC:.c=.o)
 
-all: $(NAME)
+all: $(LIBFT) $(NAME)
+
+$(LIBFT):
+	@$(MAKE) -C $(LIBFT_DIR)
 
 $(NAME): $(OBJ)
-	@$(CC) $(CFLAGS) $(OBJ) -o $(NAME)
+	@$(CC) $(CFLAGS) $(OBJ) $(LIBFT) -o $(NAME)
 	@echo "Executable $(NAME) created."
 
 %.o: %.c
@@ -26,10 +35,12 @@ $(NAME): $(OBJ)
 
 clean:
 	@rm -f $(OBJ)
+	@$(MAKE) -C $(LIBFT_DIR) clean
 	@echo "Object files cleaned."
 
 fclean: clean
 	@rm -f $(NAME)
+	@$(MAKE) -C $(LIBFT_DIR) fclean
 	@echo "Executable $(NAME) and object files cleaned."
 
 re: fclean all
--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: enpardo- <enpardo-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 20:24:37 by enpardo-          #+#    #+#             */
-/*   Updated: 2025/04/15 19:15:11 by enpardo-         ###   ########.fr       */
+/*   Updated: 2025/04/15 19:42:56 by enpardo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,50 +19,46 @@
 
 typedef struct s_stack
 {
-	int				value;
-	int				index;
-	struct s_stack	*next;
-}	t_stack;
+    int             value;
+    int             index;
+    struct s_stack  *next;
+} t_stack;
 
 // Stack operations
-void	sa(t_stack **a);
-void	sb(t_stack **b);
-void	ss(t_stack **a, t_stack **b);
-void	pa(t_stack **a, t_stack **b);
-void	pb(t_stack **a, t_stack **b);
-void	ra(t_stack **a);
-void	rb(t_stack **b);
-void	rr(t_stack **a, t_stack **b);
-void	rra(t_stack **a);
-void	rrb(t_stack **b);
-void	rrr(t_stack **a, t_stack **b);
+void    sa(t_stack **a);
+void    sb(t_stack **b);
+void    ss(t_stack **a, t_stack **b);
+void    pa(t_stack **a, t_stack **b);
+void    pb(t_stack **a, t_stack **b);
+void    ra(t_stack **a);
+void    rb(t_stack **b);
+void    rr(t_stack **a, t_stack **b);
+void    rra(t_stack **a);
+void    rrb(t_stack **b);
+void    rrr(t_stack **a, t_stack **b);
 
 // Stack utils
-t_stack	*ft_stacknew(int value);
-void	ft_stackadd_front(t_stack **stack, t_stack *new);
-int		ft_stacksize(t_stack *stack);
-void	free_stack(t_stack **stack);
-int		is_sorted(t_stack *stack);
+t_stack *ft_stacknew(int value);
+void    ft_stackadd_front(t_stack **stack, t_stack *new);
+int     ft_stacksize(t_stack *stack);
+void    free_stack(t_stack **stack);
+int     is_sorted(t_stack *stack);
 
-// Sorting
-void	sort_three(t_stack **a);
-void	sort_five(t_stack **a, t_stack **b);
-void	big_sort(t_stack **a, t_stack **b);
+// Sorting algorithms
+void    sort_three(t_stack **a);
+void    sort_five(t_stack **a, t_stack **b);
+void    quicksort(t_stack **a, t_stack **b, int size);
+void    hybrid_sort(t_stack **a, t_stack **b);
 
 // Input validation
-int		is_valid_input(char **av);
-int		has_duplicates(t_stack *a);
+int     is_valid_input(char **av);
+int     has_duplicates(t_stack *a);
+void    assign_indexes(t_stack *a);
 
-// Radix utils
-int		get_max_bits(int size);
-void	push_all_back(t_stack **a, t_stack **b);
-void	process_bit(t_stack **a, t_stack **b, int bit_pos, int size);
-
-// Utils
-int		ft_atoi(const char *str);
-long	ft_atol(const char *str);
-int		ft_isdigit(int c);
-void	ft_putstr_fd(char *s, int fd);
-void	assign_indexes(t_stack *a);
+// Utils 
+int     ft_atoi(const char *str);
+long    ft_atol(const char *str);
+int     ft_isdigit(int c);
+void    ft_putstr_fd(char *s, int fd);
 
 #endif
